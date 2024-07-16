6a4f9e3cbc74751e80c3accacd709282d075a734
matddung <matddung1111@gmail.com>
2024-07-16 20:52:13 +0900

    update

--- frontend/src/util/WekaAPIUtil.js
+++ frontend/src/util/WekaAPIUtil.js
@@ -3,49 +3,49 @@ import { request } from './APIRequest';
 
 export function getStatistics() {
     return request({
-        url: `${API_BASE_URL}/statistics`,
+        url: `${API_BASE_URL}/lotto/statistics`,
         method: 'GET'
     });
 }
 
 export function getStatisticsTop6(date) {
     return request({
-        url: `${API_BASE_URL}/top6?date=${date}`,
+        url: `${API_BASE_URL}/lotto/top6?date=${date}`,
         method: 'GET'
     });
 }
 
 export function getPatternRecognition(date) {
     return request({
-        url: `${API_BASE_URL}/pattern-recognition?date=${date}`,
+        url: `${API_BASE_URL}/lotto/pattern-recognition?date=${date}`,
         method: 'GET'
     });
 }
 
 export function generateRandomNumbers() {
     return request({
-        url: `${API_BASE_URL}/random`,
+        url: `${API_BASE_URL}/lotto/random`,
         method: 'GET'
     });
 }
 
 export function getEnsemblePrediction(date) {
     return request({
-        url: `${API_BASE_URL}/ensemble?date=${date}`,
+        url: `${API_BASE_URL}/lotto/ensemble?date=${date}`,
         method: 'GET'
     });
 }
 
 export function runMonteCarloSimulation() {
     return request({
-        url: `${API_BASE_URL}/monte-carlo`,
+        url: `${API_BASE_URL}/lotto/monte-carlo`,
         method: 'GET'
     });
 }
 
 export function getCurrentUserLottoInfo() {
     return request({
-        url: `${API_BASE_URL}/user-lotto-info`,
+        url: `${API_BASE_URL}/lotto/user-lotto-info`,
         method: 'GET'
     })
 }
