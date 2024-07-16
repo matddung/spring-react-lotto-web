97ef385668ab75e290c241648afff5579b7bb5c8
matddung <matddung1111@gmail.com>
2024-07-16 17:24:53 +0900

    fix error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +12,4 @@ root.render(
     </Router>
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -1,6 +1,6 @@
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL('http://ec2-3-39-227-55.ap-northeast-2.compute.amazonaws.com:3000', window.location.href);
+    const publicUrl = new URL('http://ec2-3-39-227-55.ap-northeast-2.compute.amazonaws.com:8080', window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
