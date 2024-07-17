528bebc587178acbf48d62c529f6e2c4ae79dcee
matddung <matddung1111@gmail.com>
2024-07-18 04:27:05 +0900

    처음부터

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,8 +1,8 @@
-export const API_BASE_URL = 'https://sbsmily.shop/api';
+export const API_BASE_URL = 'http://ec2-3-39-227-55.ap-northeast-2.compute.amazonaws.com/api';
 export const ACCESS_TOKEN = 'accessToken';
 export const REFRESH_TOKEN = 'refreshToken';
 
-export const OAUTH2_REDIRECT_URI = 'https://sbsmily.shop/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = 'http://ec2-3-39-227-55.ap-northeast-2.compute.amazonaws.com/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = `${API_BASE_URL}/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`;
 export const KAKAO_AUTH_URL = `${API_BASE_URL}/oauth2/authorize/kakao?redirect_uri=${OAUTH2_REDIRECT_URI}`;
