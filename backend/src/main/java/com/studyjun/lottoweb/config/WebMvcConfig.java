c7061e7ce792a969907924ff344bf322e83e755c
matddung <matddung1111@gmail.com>
2024-07-17 22:42:50 +0900

    update domain

--- backend/src/main/java/com/studyjun/lottoweb/config/WebMvcConfig.java
+++ backend/src/main/java/com/studyjun/lottoweb/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://sbsmily.shop")
+                        .allowedOrigins("https://sbsmily.shop:3000")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
