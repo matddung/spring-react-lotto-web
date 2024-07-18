f03bb13e121e7cec0c241e129b6d0d91fd1226e3
matddung <matddung1111@gmail.com>
2024-07-18 20:14:10 +0900

    https

--- backend/src/main/java/com/studyjun/lottoweb/config/WebMvcConfig.java
+++ backend/src/main/java/com/studyjun/lottoweb/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://sbsmily.shop:3000")
+                        .allowedOrigins("https://sbsmily.shop")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,8 +1,8 @@
-export const API_BASE_URL = 'http://sbsmily.shop:8080/api';
+export const API_BASE_URL = 'http://sbsmily.shop/api';
 export const ACCESS_TOKEN = 'accessToken';
 export const REFRESH_TOKEN = 'refreshToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://ec2-3-39-227-55.ap-northeast-2.compute.amazonaws.com:3000/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = 'http://sbsmily.shop/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = `${API_BASE_URL}/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`;
 export const KAKAO_AUTH_URL = `${API_BASE_URL}/oauth2/authorize/kakao?redirect_uri=${OAUTH2_REDIRECT_URI}`;
