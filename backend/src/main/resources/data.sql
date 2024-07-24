ce06babefe99e8f5a03a8a3bfe549d9bce7eac9d
matddung <matddung1111@gmail.com>
2024-07-25 07:12:10 +0900

    add data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,7 +8,7 @@ USE `lotto`;
 
 /*Data for the table `user` */
 
-insert  into `user`(`id`,`email`,`nickname`,`password`,`provider`,`provider_id`,`role`) values 
+insert IGNORE into `user`(`id`,`email`,`nickname`,`password`,`provider`,`provider_id`,`role`) values
 (1,admin@admin.a,관리자,$2a$10$yt/QG50osgem7Ndg1tyFbOnveW1B1bJg0F4sywQqKZFqt/eXAo9Dy,0,local.admin,ADMIN),
 (2,string@aa.bb,string,$2a$10$Gg1Ve5JLKYlcphuDSvzcI.wILD0FwnIsyPilBM5vGvAKnnLLHtwYa,0,NULL,USER),
 (3,string1@aa.bb,string1,$2a$10$XlwI5XkCuQbtuYaN7gtae.l64xiZajesEfjsCUFl/dK1xqS3oXiWu,0,NULL,USER);
